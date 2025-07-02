355bf1bdc61710768e642de89b75badd623887ea
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-07-01 20:13:20 -0500

    Update UserManagmentApplication.java
    Delete dotenv

--- src/main/java/com/quantumdev/integraservicios/userManagment/UserManagmentApplication.java
+++ src/main/java/com/quantumdev/integraservicios/userManagment/UserManagmentApplication.java
@@ -1,15 +1,11 @@
 package com.quantumdev.integraservicios.userManagment;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagmentApplication {
     public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(UserManagmentApplication.class, args);
     }
 }
