9ee28eaef7f9547883509893010b3474b3b6ccde
Nicolás Sabogal <mew-four@hotmail.com>
2025-07-06 21:46:19 -0500

    feat: add frontend url environment variable for CORS configuration.

--- src/main/java/com/quantumdev/integraservicios/userManagment/Config/WebConfig.java
+++ src/main/java/com/quantumdev/integraservicios/userManagment/Config/WebConfig.java
@@ -4,12 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.env.Environment;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer{
+
+    private final Environment environment;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")  // Cambia esta URL a la de tu frontend
+                .allowedOrigins(environment.getProperty("frontend.url"))  // Cambia esta URL a la de tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowedHeaders("*")  // Permitir todos los encabezados
                 .allowCredentials(true);
